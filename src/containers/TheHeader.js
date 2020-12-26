b1715b25566471ab847255c57ac7d98eb74059f6
Tim Luu <timluu105@gmail.com>
2020-12-26 15:22:17 -0800

    Top Right Navigation Template Work

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -2,18 +2,27 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
 	CHeader,
+	CToggler,
 	CHeaderBrand,
 	CHeaderNav,
 	CHeaderNavItem,
 	CHeaderNavLink,
 	CSubheader,
-	CToggler,
 	CBreadcrumbRouter,
+	CLink,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
+import { freeSet } from "@coreui/icons";
 
 // routes config
 import routes from "../routes/index";
+
+import {
+	TheHeaderDropdown,
+	TheHeaderDropdownMssg,
+	TheHeaderDropdownNotif,
+	TheHeaderDropdownTasks,
+} from "./index";
 
 const TheHeader = () => {
 	const dispatch = useDispatch();
@@ -48,12 +57,18 @@ const TheHeader = () => {
 				onClick={toggleSidebar}
 			/>
 			<CHeaderBrand className="mx-auto d-lg-none" to="/">
-				<CIcon name="logo" height="48" alt="Logo" />
+				<CIcon content={freeSet.logo} height="48" alt="Logo" />
 			</CHeaderBrand>
 
 			<CHeaderNav className="d-md-down-none mr-auto">
 				<CHeaderNavItem className="px-3">
 					<CHeaderNavLink to="/dashboard">Dashboard</CHeaderNavLink>
+				</CHeaderNavItem>
+				<CHeaderNavItem className="px-3">
+					<CHeaderNavLink to="/users">Users</CHeaderNavLink>
+				</CHeaderNavItem>
+				<CHeaderNavItem className="px-3">
+					<CHeaderNavLink>Settings</CHeaderNavLink>
 				</CHeaderNavItem>
 			</CHeaderNav>
 
@@ -65,22 +80,31 @@ const TheHeader = () => {
 					title="Toggle Light/Dark Mode"
 				>
 					<CIcon
-						name="cil-moon"
+						content={freeSet.cilMoon}
 						className="c-d-dark-none"
 						alt="CoreUI Icons Moon"
 					/>
 					<CIcon
+						content={freeSet.cilSun}
 						name="cil-sun"
 						className="c-d-default-none"
 						alt="CoreUI Icons Sun"
 					/>
 				</CToggler>
+				<TheHeaderDropdownNotif />
+				<TheHeaderDropdownTasks />
+				<TheHeaderDropdownMssg />
+				<TheHeaderDropdown />
 				<CToggler
 					inHeader
 					className="d-md-down-none"
 					onClick={() => dispatch({ type: "SET_ASIDE", asideShow: !asideShow })}
 				>
-					<CIcon className="mr-2" size="lg" name="cil-applications-settings" />
+					<CIcon
+						className="mr-2"
+						content={freeSet.cilApplicationsSettings}
+						size="lg"
+					/>
 				</CToggler>
 			</CHeaderNav>
 
@@ -89,6 +113,23 @@ const TheHeader = () => {
 					className="border-0 c-subheader-nav m-0 px-0 px-md-3"
 					routes={routes}
 				/>
+				<div className="d-md-down-none mfe-2 c-subheader-nav">
+					<CLink className="c-subheader-nav-link" href="#">
+						<CIcon content={freeSet.cilSpeech} alt="Settings" />
+					</CLink>
+					<CLink
+						className="c-subheader-nav-link"
+						aria-current="page"
+						to="/dashboard"
+					>
+						<CIcon content={freeSet.cilGraph} alt="Dashboard" />
+						&nbsp;Dashboard
+					</CLink>
+					<CLink className="c-subheader-nav-link" href="#">
+						<CIcon content={freeSet.cilSettings} alt="Settings" />
+						&nbsp;Settings
+					</CLink>
+				</div>
 			</CSubheader>
 		</CHeader>
 	);
